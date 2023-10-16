21b9456d3fcaaf2941b89ad0392a4fe834b6ec6f
ahmedmokhtar2030 <shoptown2022@gmail.com>
2023-10-16 16:48:27 +0300

    asdsa

--- src/redux/reducers/AuthReducer.js
+++ src/redux/reducers/AuthReducer.js
@@ -1,7 +1,6 @@
 import { SETAUTHDATA, RESETAUTHDATA, SETAUTHROUTE } from './../actions/types';
 import jwt from 'jwt-decode' 
 const token_auth = localStorage.getItem('token') ? jwt(localStorage.getItem('token')).auth : false;
-
 const AuthReducer = (state={AuthData:{username:'', role:''}, auth:token_auth }, action)=>{
    switch (action.type) {
     case SETAUTHROUTE:
